f6bcf6490948a727c4d184646f5df8179fe54274
Tim Miller <mille449@gmail.com>
2010-04-01 15:43:25 -0400

    changed dob format in dummy data

--- sqlscripts/3_3_10/dummyData.sql
+++ sqlscripts/3_3_10/dummyData.sql
@@ -4,7 +4,7 @@ TRUNCATE admins;
 TRUNCATE users;
 
 INSERT INTO users VALUES (NULL, 'employee','pass');
-INSERT INTO employees VALUES (1, 'John', 'M', 'Doe', '01-01-1979', 'johndoe@example.com', 'College', NULL);
+INSERT INTO employees VALUES (1, 'John', 'M', 'Doe', '01/01/1979', 'johndoe@example.com', 'College', NULL);
 
 INSERT INTO users VALUES (NULL, 'employer','pass');
 INSERT INTO employers VALUES (2, 'ABC Corp.', '100 Main St.', 'Springfield', 'MI', '48825', 'jobs@abccorp.com', '555-345-3456', 'www.abccorp.com/jobs', 'Marketing', 'At ABC Corp we market things.');
